06852e51c19bb0a7259e1b09bbb9aea71bf498c9
davidauza.engineer <davidauza01@gmail.com>
2019-10-04 06:14:46 -0500

    Add variables and types

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -5,11 +5,19 @@
 //  Created by David Auza on 9/27/19.
 //  Copyright © 2019 David Auza. All rights reserved.
 //
+// This is a comment - I can put anything I want here
+// Now this is a comment. Xcode is getting confused
+
+/* This is a multi-line comment
+   It keeps going until I close
+   the comment with this matching symbol */
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *testLabel;
+
 @property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
@@ -22,14 +30,25 @@
 }
 
 - (IBAction)testButtonTapped:(id)sender {
-    NSString *worked = @"It worked!";
-    if ([self.testLabel.text  isEqual: worked]) {
-        self.testLabel.text = @"Click the button below";
+    NSString *standardText = @"Click the button below";
+    if ([self.testLabel.text isEqual:standardText]) {
+        int heightFeet = 5;
+        int heightInches = 11; // I'm 5ft 11in
+        double heightCm = ((heightFeet * 12) + heightInches) * 2.54;
+        self.testLabel.text = [NSString stringWithFormat:@"%f", heightCm];
+        [_button setTitle:@"Go back!" forState:UIControlStateNormal];
+    } else {
+        self.testLabel.text = standardText;
         [_button setTitle:@"Click here!" forState:UIControlStateNormal];
-        return;
     }
-    self.testLabel.text = worked;
-    [_button setTitle:@"Go back" forState:UIControlStateNormal];
+//    NSString *worked = @"It worked!";
+//    if ([self.testLabel.text  isEqual: worked]) {
+//        self.testLabel.text = @"Click the button below";
+//        [_button setTitle:@"Click here!" forState:UIControlStateNormal];
+//        return;
+//    }
+//    self.testLabel.text = worked;
+//    [_button setTitle:@"Go back" forState:UIControlStateNormal];
 }
 
 @end
