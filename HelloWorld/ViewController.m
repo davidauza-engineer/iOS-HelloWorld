aca674fd61a6a12018262c9f2a3405ecaeedb5d6
davidauza.engineer <davidauza01@gmail.com>
2019-10-05 07:09:42 -0500

    Clean code

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -5,12 +5,6 @@
 //  Created by David Auza on 9/27/19.
 //  Copyright © 2019 David Auza. All rights reserved.
 //
-// This is a comment - I can put anything I want here
-// Now this is a comment. Xcode is getting confused
-
-/* This is a multi-line comment
-   It keeps going until I close
-   the comment with this matching symbol */
 
 #import "ViewController.h"
 
@@ -42,20 +36,11 @@
         } else {
             self.testLabel.text = @"You are under 1 meter tall!";
         }
-//        self.testLabel.text = [NSString stringWithFormat:@"%f", heightCm];
         [_button setTitle:@"Go back!" forState:UIControlStateNormal];
     } else {
         self.testLabel.text = standardText;
         [_button setTitle:@"Click here!" forState:UIControlStateNormal];
     }
-//    NSString *worked = @"It worked!";
-//    if ([self.testLabel.text  isEqual: worked]) {
-//        self.testLabel.text = @"Click the button below";
-//        [_button setTitle:@"Click here!" forState:UIControlStateNormal];
-//        return;
-//    }
-//    self.testLabel.text = worked;
-//    [_button setTitle:@"Go back" forState:UIControlStateNormal];
 }
 
 @end
